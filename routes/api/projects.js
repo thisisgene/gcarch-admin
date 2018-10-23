ab1b23f51b9eda47b4b0485c47a6c74a9992fe9c
thisisgene <emdo2000@gmail.com>
2018-10-23 18:36:25 +0200

    Added project input validation and update functionality.

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
+const marked = require('marked')
 
 // Load project model
 const Project = require('../../models/Project')
 const User = require('../../models/User')
 
-// Load validation
+// Load input validation
 const validateProjectInput = require('../../validation/project')
 
 // @route   GET api/projects/test
@@ -18,7 +19,7 @@ router.get('/test', (req, res) => res.json({ msg: 'Projects Works' }))
 module.exports = router
 
 // @route   GET api/projects
-// @desc    Get projects
+// @desc    Get all projects
 // @access  Private
 router.get(
   '/',
@@ -66,16 +67,87 @@ router.post(
           projectFields.title = body.name
           projectFields.handle = body.name.replace(/\s/g, '_')
         }
-        const newProject = new Project({
-          name: projectFields.name,
-          title: projectFields.title,
-          handle: projectFields.handle
-        })
-        newProject
+        new Project(projectFields)
           .save()
           .then(project => res.json(project))
           .catch(err => res.json(err))
       }
     })
+  }
+)
+
+// @route   GET api/projects/:id
+// @desc    Get project by id
+// @access  Private
+router.get(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {}
+    Project.findById(req.params.id)
+      .then(project => {
+        if (!project) {
+          errors.noprojects = 'Kein Projekt mit dieser ID.'
+          return res.status(404).json(errors.noprojects)
+        }
+        res.json(project)
+      })
+      .catch(err => res.status(404).json(err))
+  }
+)
+
+// @route   POST api/projects/update/:id
+// @desc    Update project by id.
+// @access  Private
+router.post(
+  '/update/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const body = req.body
+
+    const { errors, isValid } = validateProjectInput(body)
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
+
+    // Check if project.name already exists
+    Project.findById(req.params.id)
+      .then(project => {
+        if (!project) {
+          errors.project = 'Projekt nicht gefunden.'
+          return res.status(400).json(errors)
+        } else {
+          // Get fields
+          const projectFields = {}
+          if (body.name) projectFields.name = body.name
+          if (body.title) projectFields.title = body.title
+          if (body.handle) projectFields.handle = body.handle
+          if (body.descriptionMarkdown) {
+            projectFields.descriptionMarkdown = body.descriptionMarkdown
+            projectFields.descriptionHtml = marked(body.descriptionMarkdown)
+          }
+          if (body.topTenOnGrid) projectFields.topTenOnGrid = body.topTenOnGrid
+          if (body.positionOnGrid)
+            projectFields.positionOnGrid = body.positionOnGrid
+          if (body.typeOfFormatOnGrid)
+            projectFields.typeOfFormatOnGrid = body.typeOfFormatOnGrid
+          if (body.importanceOnGrid)
+            projectFields.importanceOnGrid = body.importanceOnGrid
+          if (body.sizeOnGrid) projectFields.sizeOnGrid = body.sizeOnGrid
+          if (body.isVisible) projectFields.isVisible = body.isVisible
+          console.log(projectFields)
+          Project.findByIdAndUpdate(
+            req.params.id,
+            { $set: projectFields },
+            { new: true }
+          ).then(project => res.json(project))
+        }
+      })
+      .catch(err => {
+        errors.project = 'Projekt nicht gefunden.'
+        return res.status(404).json(errors)
+      })
   }
 )
--- validation/project.js
+++ validation/project.js
@@ -5,11 +5,21 @@ module.exports = function validateProjectInput(data) {
   let errors = {}
 
   data.name = !isEmpty(data.name) ? data.name : ''
+  data.title = !isEmpty(data.title) ? data.title : ''
+  data.handle = !isEmpty(data.handle) ? data.handle : ''
 
   if (Validator.isEmpty(data.name)) {
     errors.name = 'Name darf nicht leer sein.'
   }
-
+  if (Validator.isEmpty(data.title)) {
+    errors.title = 'Titel darf nicht leer sein.'
+  }
+  if (Validator.isEmpty(data.handle)) {
+    errors.handle = 'Handle darf nicht leer sein.'
+  }
+  if (!Validator.isLength(data.handle, { min: 3, max: 40 })) {
+    errors.handle = 'Handle muss zwischen 3 und 40 Zeichen lang sein.'
+  }
   // Only alphanumeric and spaces
   if (!Validator.isAlphanumeric(Validator.blacklist(data.name, ' '), 'de-DE')) {
     errors.name = 'Name darf keine Sonderzeichen enthalten.'
