a61de3df3dcae0f7b3e4ea9caa5e82dd2a2d1aff
Eugene Delta <eugenedelta@Eugenes-iMac.local>
2018-11-30 17:58:14 +0100

    Added news section for user.

--- client/src/components/user/dashboard/news/News.js
+++ client/src/components/user/dashboard/news/News.js
@@ -1,31 +1,60 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import store from '../../../../store'
 import {
   clearCurrentProject,
+  getAllProjects,
   hasBackgroundImage
 } from '../../../../actions/projectActions'
 
+import styles from './News.module.sass'
+
 class News extends Component {
   componentDidMount() {
-    this.props.hasBackgroundImage(false)
     store.dispatch(clearCurrentProject())
+    this.props.hasBackgroundImage(false)
+    if (!this.props.project.projects) {
+      this.props.getAllProjects()
+    }
   }
   render() {
-    return (
-      <div>
-        <p>Aktuell</p>
-      </div>
-    )
+    const { projects } = this.props.project
+    let projectList = []
+    if (projects) {
+      projectList = projects.map(project => {
+        return (
+          <div key={project._id} className={styles['news-item']}>
+            <NavLink
+              className={styles['link']}
+              to={`/user/projekte/${project._id}`}
+            >
+              <img
+                src={`/public/${project._id}/${project.images[0].originalName}`}
+                alt=""
+              />
+              <div className={styles['news-info']}>
+                <div className={styles['news-info-name']}>{project.name}</div>
+                <div
+                  className={styles['news-info-description']}
+                  dangerouslySetInnerHTML={{ __html: project.descriptionHtml }}
+                />
+              </div>
+            </NavLink>
+          </div>
+        )
+      })
+    }
+    return <div className={styles['news-container']}>{projectList}</div>
   }
 }
 
 const mapStateToProps = state => ({
-  // hasBackgroundImage: state.hasBackgroundImage
+  project: state.project
 })
 
 export default connect(
   mapStateToProps,
-  { hasBackgroundImage }
+  { hasBackgroundImage, getAllProjects }
 )(News)
