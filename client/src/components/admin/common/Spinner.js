821816a7fbbde13867bae464f5cdb63820bed6e9
thisisgene <emdo2000+genegit@gmail.com>
2019-01-28 17:04:13 +0100

    Moved spinner in the center

--- client/src/components/admin/common/Spinner.js
+++ client/src/components/admin/common/Spinner.js
@@ -3,7 +3,18 @@ import spinner from './spinner.gif'
 
 export default function Spinner() {
   return (
-    <div>
+    <div
+      style={{
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
       <img
         src={spinner}
         alt="Loading..."
