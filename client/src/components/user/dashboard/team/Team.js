ed0871f25d486dff8bc82761acfa64da715435a1
thisisgene <emdo2000+genegit@gmail.com>
2019-02-08 17:06:11 +0100

    Added collapsable team info

--- client/src/components/user/dashboard/team/Team.js
+++ client/src/components/user/dashboard/team/Team.js
@@ -9,48 +9,74 @@ import {
 
 import { teamData } from './teamData'
 
+import cx from 'classnames'
 import styles from './Team.module.sass'
 
-import imgBertram from '../../common/img/bertram.png'
-
 class Team extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showBio: []
+    }
+  }
   componentDidMount() {
     store.dispatch(clearCurrentProject())
     this.props.hasBackgroundImage(false)
+    const showBio = []
+    teamData &&
+      teamData.map(item => {
+        showBio[item.name] = false
+      })
+    this.setState({
+      showBio: showBio
+    })
+    console.log(showBio)
   }
 
-  onBioClick = e => {}
+  // componentDidUpdate()
+
+  onBioClick = e => {
+    console.log(this.state.showBio, e.target.name)
+    const showBio = this.state.showBio
+    showBio[e.target.name] = !showBio[e.target.name]
+    this.setState({
+      showBio: showBio
+    })
+  }
 
   render() {
-    let teamList = []
-    if (teamData) {
-      for (const [index, value] of Object.entries(teamData)) {
-        teamList.push(
-          <div key={index} className={styles['team-member']}>
-            <img src={value.img} alt="" />
-            <div className={styles['team-member-info']}>
-              <div className={styles['team-member-name']}>{value.name}</div>
-              <div className={styles['team-member-email']}>{value.email}</div>
-              {value.description && (
-                <div className={styles['biographie']}>
-                  <div
-                    className={styles['biographie--link']}
-                    onClick={this.onBioClick}
-                  >
-                    Kurzbiographie
+    return (
+      <div className={styles['team-container']}>
+        {teamData &&
+          teamData.map((item, index) => (
+            <div key={index} className={styles['team-member']}>
+              <img src={item.img} alt="" />
+              <div className={styles['team-member-info']}>
+                <div className={styles['team-member-name']}>{item.name}</div>
+                <div className={styles['team-member-email']}>{item.email}</div>
+                {item.description && (
+                  <div className={styles['biographie']}>
+                    <button
+                      className={styles['biographie--link']}
+                      onClick={this.onBioClick}
+                      name={item.name}
+                    >
+                      Kurzbiographie
+                    </button>
+                    <div
+                      className={cx(styles['team-member-description'], {
+                        [styles['visible']]: this.state.showBio[item.name]
+                      })}
+                    >
+                      {item.description}
+                    </div>
                   </div>
-                  <div className={styles['team-member-description']}>
-                    {value.description}
-                  </div>
-                </div>
-              )}
+                )}
+              </div>
             </div>
-          </div>
-        )
-      }
-    }
-
-    return <div className={styles['team-container']}>{teamList}</div>
+          ))}
+      </div>
+    )
   }
 }
 export default connect(
