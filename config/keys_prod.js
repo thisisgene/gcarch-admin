55c1f93d69f282917bc73fa23b23489ea18b2b5b
thisisgene <emdo2000+genegit@gmail.com>
2019-10-17 13:57:12 +0200

    Added

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,5 +2,6 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
   s3key: process.env.S3KEY,
-  s3secret: process.env.S3SECRET
+  s3secret: process.env.S3SECRET,
+  REACT_APP_GOOGLE_KEY: process.env.REACT_APP_GOOGLE_KEY
 }
